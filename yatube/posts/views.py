3d0586c0a553cc1acd4fd3bb4e7cdc5ae427077b
Искандер Рыскулов <logick7@mail.ru>
2022-06-16 19:21:51 +0300

    4.7

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -84,8 +84,7 @@ def post_edit(request, post_id):
         return redirect('posts:post_detail', post_id)
     form = PostForm(
         request.POST or None,
-        instance=post,
-        )
+        instance=post)
     if form.is_valid():
         form.save()
         return redirect('posts:post_detail',
