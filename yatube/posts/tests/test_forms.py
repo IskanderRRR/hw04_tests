042a0e4304b9aaa6f87754aedccc709ae1966829
Искандер Рыскулов <logick7@mail.ru>
2022-06-18 10:45:29 +0300

    0.6

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -1,13 +1,11 @@
 import shutil
 import tempfile
-from tokenize import group
 
 from django.conf import settings
 from django.test import Client, TestCase, override_settings
 from django.urls import reverse
 
 from ..models import Group, Post, User
-
 
 TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
 
